0dfb5753196d8bdcebd55a34ae9afd1428fdca85
spl211 <bardabun>
2021-01-08 16:25:20 +0200

    dabun

---
+++ include/Read.h
@@ -0,0 +1,8 @@
+//
+// Created by spl211 on 08/01/2021.
+//
+
+#ifndef CLIENT_READ_H
+#define CLIENT_READ_H
+
+#endif //CLIENT_READ_H
---
+++ include/Write.h
@@ -0,0 +1,8 @@
+//
+// Created by spl211 on 08/01/2021.
+//
+
+#ifndef CLIENT_WRITE_H
+#define CLIENT_WRITE_H
+
+#endif //CLIENT_WRITE_H
