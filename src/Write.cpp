93f04ea831a081b191182d262a7caba3dbb86d4c
mamanyon <72416517+mamanyon@users.noreply.github.com>
2021-01-09 16:13:49 +0200

    Update Write.cpp

--- src/Write.cpp
+++ src/Write.cpp
@@ -17,32 +17,78 @@ bool Write::write() {
         std::string message;
         bool isThereMoreMessage = true;
         int indexMessage = 0;
-        if (line == "LOGOUT" || line == "MYCOURSES") {
+        short opcode;
+        bool sent;
+        if (line == "LOGOUT") {
+            opcode = 4;
             isThereMoreMessage = false;
             message = line;
         }
-        if (isThereMoreMessage) {
-            while (line[indexMessage] != ' '){
-               message+= line[indexMessage];
+        if (line == "MYCOURSES") {
+            opcode = 11;
+            isThereMoreMessage = false;
+            message = line;
+        }
+
+        if (!isThereMoreMessage) { // then must be 4 | 11
+            sent = connectionHandler->sendFrame(opcode);
+        } else { // more message
+            while (line[indexMessage] != ' ') {
+                message += line[indexMessage];
                 indexMessage++;
-        }
-            std::string restOfTheMessage;
-            if(message== "ADMINREG"){
-                restOfTheMessage= line.substr(indexMessage+1);
-               // connectionHandler.sendFrame(restOfTheMessage,1);
-                //a func we need to make - gets string and short opcode
-                //
             }
-            if(message==" "){}
-            if(message==" "){}
-            if(message==" "){}
-            if(message==" "){}
-            if(message==" "){}
+            std::string restOfTheMessage = line.substr(indexMessage + 1);
+            //1,2,3,8 same func
+            if (message == "ADMINREG") {
+                //rest of message is "username pass"
+                sent = connectionHandler->sendFrame(restOfTheMessage, 1);
+            } else if (message == "STUDENTREG") {
+                //rest of message is "username pass"
+                sent = connectionHandler->sendFrame(restOfTheMessage, 2);
+            } else if (message == "LOGIN") {
+                //rest of message is "username pass"
+                sent = connectionHandler->sendFrame(restOfTheMessage, 3);
+            } else if (message == "STUDENTSTAT") {
+                sent = connectionHandler->sendFrame(restOfTheMessage, 8);
+            } else
+                //5 ,6 ,7, 9 ,10 same func getting op and course num .
+            if (message == "COURSEREG") {
+                //rest of message is coursenum
+                std::stringstream courseNum(restOfTheMessage);
+                short cNum = 0;
+                courseNum >> cNum;  // converting string to short
+                sent = connectionHandler->sendFrameShort(cNum, 5);
+            } else if (message == "KDAMCHECK") {
+                std::stringstream courseNum(restOfTheMessage);
+                short cNum = 0;
+                courseNum >> cNum;  // converting string to short
+                sent = connectionHandler->sendFrameShort(cNum, 6);
+            } else if (message == "COURSESTAT") {
+                std::stringstream courseNum(restOfTheMessage);
+                short cNum = 0;
+                courseNum >> cNum;  // converting string to short
+                sent = connectionHandler->sendFrameShort(cNum, 7);
+            } else if (message == "ISREGISTERED") {
+                std::stringstream courseNum(restOfTheMessage);
+                short cNum = 0;
+                courseNum >> cNum;  // converting string to short
+                sent = connectionHandler->sendFrameShort(cNum, 9);
+            } else if (message == "UNREGISTER") {
+                std::stringstream courseNum(restOfTheMessage);
+                short cNum = 0;
+                courseNum >> cNum;  // converting string to short
+                sent = connectionHandler->sendFrameShort(cNum, 10);
+            }
 
 
         }
-        return false;
+        if (!sent) {
+            std::cout << "Disconnected. Exiting... write \n" << std::endl;
+            break;
+        }
+
     }
+    return false;
 }
 
 
